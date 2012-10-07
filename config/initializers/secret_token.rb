1ea2ce8427b542bea95e8e4327e163176303f6b5
Adam MacLeod <adam@macleod.id.au>
2012-10-08 09:31:21 +1100

    Added new empty rails project. Added default rvmrc for @playing-with-data.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayingWithData::Application.config.secret_token = 'db4b0888e34c2552839452230dd4fbee7e6947c020e12e4c260bbb19046d9358405b863b1ad4eecd6bfbdfc0abcd2ea585cc87e566bacc7a898b538eeab0b774'
