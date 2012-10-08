24bb9e970f11cd7343ed06bd8a1ba2205bdcf0b6
Adam MacLeod <adam@macleod.id.au>
2012-10-08 13:00:05 +1100

    Added validation to pass tests.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,6 @@ class Bill < ActiveRecord::Base
   attr_accessible :csv
 
   has_many :calls
+
+  validates :csv, :presence => true
 end
--- app/models/call.rb
+++ app/models/call.rb
@@ -1,8 +1,17 @@
 class Call < ActiveRecord::Base
   belongs_to :bill
-  attr_accessible :cost, :datetime, :destination, :duration, :source
+  attr_accessible :source, :destination, :datetime, :duration, :cost
 
   # Handling Money:
   # http://stackoverflow.com/questions/1019939/ruby-on-rails-best-method-of-handling-currency-money
   # https://github.com/tobi/money_column
+
+  validates :bill, :presence => true
+  validates :source, :presence => true
+  validates :destination, :presence => true
+  validates :datetime, :presence => true
+  validates :duration, :presence => true
+  validates :cost, :presence => true
+  validates_numericality_of :cost, :greater_than_or_equal_to => 0
+  validates_numericality_of :duration, :greater_than_or_equal_to => 0
 end
