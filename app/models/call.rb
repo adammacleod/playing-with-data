732ef66623d6252853c6289345c00163c434f2c6
Adam MacLeod <adam@macleod.id.au>
2012-10-08 11:27:04 +1100

    Added a link for dealing with money values.

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,4 +1,8 @@
 class Call < ActiveRecord::Base
   belongs_to :bill
   attr_accessible :cost, :datetime, :destination, :duration, :source
+
+  # Handling Money:
+  # http://stackoverflow.com/questions/1019939/ruby-on-rails-best-method-of-handling-currency-money
+  # https://github.com/tobi/money_column
 end
