f4237759808097eb42a30e89989854ab584a00b5
Adam MacLeod <adam@macleod.id.au>
2012-10-11 10:57:54 +1100

    Fixed ugly code.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -11,28 +11,15 @@ class Bill < ActiveRecord::Base
   def process_csv
     CSV.open(self.csv.current_path, { :headers => :first_row, :header_converters => :symbol } ) do |csv|
       csv.each do |row|
-        call = Call.new()
-        call.source = row[:source]
-        call.destination = row[:destination]
-        call.date = row[:date]
-        call.time = row[:time]
-        call.duration = row[:duration]
-        call.cost = row[:cost]
-        call.bill = self
+        call = self.calls.build(row.to_hash)
         unless call.save
-          ic = self.invalid_calls.build()
-          ic.source = row[:source]
-          ic.destination = row[:destination]
-          ic.date = row[:date]
-          ic.time = row[:time]
-          ic.duration = row[:duration]
-          ic.cost = row[:cost]
+          invalid = self.invalid_calls.build(row.to_hash)
 
-          ic.error_messages = call.errors
-          ic.lineno = csv.lineno
+          invalid.error_messages = call.errors
+          invalid.lineno = csv.lineno
 
-          ic.csv_source = row.to_s
-          ic.save
+          invalid.csv_source = row.to_s
+          invalid.save
         end
       end
     end
